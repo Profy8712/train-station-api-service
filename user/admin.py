63a7a8f17414c692e080df127089b8388d4c5a09
Oleksandr Kurin <profysss8712@gmail.com>
2025-05-25 18:27:07 +0300

    setting user app with JWT authÐ

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
-
-# Register your models here.
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+    )
+    list_display = ("email", "first_name", "last_name", "is_staff")
--- user/models.py
+++ user/models.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 class User(AbstractUser):
+    email = models.EmailField(_("email address"), unique=True)
     username = None
-    email = models.EmailField(unique=True)
-    USERNAME_FIELD = 'email'
+
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
 
